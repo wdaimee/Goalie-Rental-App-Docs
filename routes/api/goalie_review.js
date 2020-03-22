717ca9f152f7a9dbf0242ed1239db688fb0d1f6c
Waqas D <wdaimee@gmail.com>
2020-03-22 19:12:20 -0400

    removed show and index from routes

--- routes/api/goalie_review.js
+++ routes/api/goalie_review.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const goalies_reviewCtrl = require('../../controllers/api/goalie_reviews');
 
-router.get('/:id/reviews', goalies_reviewCtrl.index);
 router.post('/:id/reviews', goalies_reviewCtrl.create);
 router.delete('/reviews/:id', goalies_reviewCtrl.delete);
 router.put('/reviews/:id', goalies_reviewCtrl.update);
